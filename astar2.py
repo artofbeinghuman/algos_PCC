7b22ae73214579b08c64396efa39925637c04eb6
Daetheys <45879037+Daetheys@users.noreply.github.com>
2019-01-31 23:57:53 +0100

    Add files via upload

--- astar2.py
+++ astar2.py
@@ -1,8 +1,7 @@
 # ----------------------------------------------------------------
 #
 #                       Algorithme Astar   1.0
-#   -> Je n'ai pas encore testé sur les graphes du prof
-# Si vous avez des idées d'optimisation c'est tout benef.
+# 
 # ----------------------------------------------------------------
 
 
@@ -10,6 +9,7 @@ infty = 10**15
 
 from importgraph import *
 import copy
+from parcours import tas as File
 
 def search(L,e,comp):
     """ Algorithme de recherche par dichotomie selon la première composante """
@@ -21,8 +21,9 @@ def search(L,e,comp):
 def get(d,cle):
     return d[cle]
 
-class File:
+class File3:
     l = None #De la forme [priority,key],...
+    d = None #Dictionnaire pour savoir qui est où dans la file
 
     def __init__(self):
         self.l = []
@@ -64,70 +65,6 @@ class File:
             if k == v:
                 return True
         return False
-        
-
-class File2:
-    """ Implémente une file (pile fifo) -> utiliser un tas"""
-    l = None #Les entrées sont sous la forme : (priorité,val)
-    d = None #Un dictionnaire des emplacements des éléments dans l
-
-    def __repr__(self):
-        """ Sert a afficher self.l quand on fait print(file)"""
-        return str(self.l)
-    
-    def __init__(self):
-        self.l = []
-        self.d = {}
-
-    def upper_propagation(self,index):
-        """ Reforme un tas en remontant à partir de l'index : O(log n)"""
-        if self.l[index][1] < self.l[index//2][1]:
-            (cle1,v1) = self.l[index]
-            (cle2,v2) = self.l[index//2]
-            self.d[cle1] = index//2
-            self.d[cle2] = index
-            (self.l[index],self.l[index//2]) = copy.copy((self.l[index//2],self.l[index]))
-            return self.upper_propagation(index//2)
-        else:
-            return index
-
-    def force_upper_propagation(self,index):
-        while index != 0:
-            index = self.upper_propagation(index)//2
-        return 0
-        
-    def add(self,e):
-        """ Ajoute un élément dans la file : O(log n) """
-        self.l.append(e)
-        index = self.upper_propagation(len(self.l)-1)
-        self.d[e[0]] = index
-
-    def modify(self,cle,v):
-        """ Modifie la clé d'une entrée en O(log n)"""
-        index = self.d[cle]
-        self.l[index] = (self.l[index][0],v)
-        new_index = self.upper_propagation(index)
-
-    def pop(self):
-        """ Récupère l'élément de plus grande priorité : O(log² n) """
-        e = self.l[0]
-        self.l[0] = (e[0],infty)
-        while self.d[e[0]] != len(self.l)-1:
-            new_index = self.force_upper_propagation(len(self.l)-1)
-        del self.l[-1]
-        return e
-
-    def empty(self):
-        """ Test si la file est vide : O(1) """
-        return self.l == []
-
-    def contains(self,v):
-        """ Test si la file contient un élément : O(1) """
-        try:
-            get(self.d,v)  #Il peut valoir 0 aussi
-            return True
-        except KeyError:
-            return False
 
 def h(x): #Difficile de donner de bonnes heuristiques sans hypothèses sur les graphes
     """ Comme A* est surtout utilisé pour le path finding dans le plan, on utilise souvent :
@@ -136,7 +73,7 @@ l'hypothèse que l'on travail sur le plan. J'avais aussi pensé à utiliser l'hy
 graphe respecte l'inégalité triangulaire afin d'utiliser la même fonction à un coefficient
 multiplicatif près qui pourrait être la valeur du plus petit arc strictement positif (en
 supposant qu'il n'y a pas d'arc négatifs"""
-    return 0 #Ici je suppose qu'il n'y a pas de cycle négatif (sinon il faut mettre -infini)
+    return 0 #Ici je suppose qu'il n'y a pas de cycle négatif
 
 # Il faut renseigner h en global !!!
 def Astar(G,s,g):
@@ -150,34 +87,25 @@ def Astar(G,s,g):
     F.add((s,cle(s)))
     T = []
     while not(F.empty()):
-        u = F.pop()[1]
-        #print(F)
+        u = F.pop()
         T.append(u)
         if u == g:
             return (d,P)
         for (v,w) in G[u]:
-            #print(u,v)
-            #print(u,v,w)
             cst = d[u] + w
             if F.contains(v):
                 if d[v] > cst:
                     d[v] = cst
                     P[v] = u
-                    #print("modif")
-                    #print("modify",cle(v),v)
                     F.modify(cle(v),v)
             elif v in T:
                 if d[v] > cst:
                     d[v] = cst
                     P[v] = u
-                    #print("add")
-                    #print("add",cle(v),v)
                     F.add((cle(v),v))
             else:
                 d[v] = cst
                 P[v] = u
-                #print("add")
-                #print("add",cle(v),v)
                 F.add((cle(v),v))
     return (False,None)
 
@@ -192,15 +120,13 @@ def test():
     import bellman as bm
     #Definition du graphe
     G = import_graph("graphs\CachanGraphe5.txt")
-    #print(G[0])
+    (bfb,bfd,bfPi) = bm.BellmanFord(G,0)
     for i in range(1,1500):
-        (bfb,bfd,bfPi) = bm.BellmanFord(G,0)
         ast = Astar(G,0,i)
         ras = ast[0][i]
         rbf = bfd[i]
-        #print(ast[0][0::10])
         if rbf != ras:
             print("Error : "+str(i)+" "+str(ras)+" "+str(rbf))
         print(get_predecessor(ast[1],0,i),get_predecessor(bfPi,0,i))
             
-
+#test()
--- dijkstra.py
+++ dijkstra.py
@@ -1,13 +1,23 @@
 import numpy as np
+#from astar2 import File
+from astar2 import Astar
 from parcours import tas as File
 
 def Dijkstra(G,s):
+    #Algorithme de Dijkstra avec des tas classique (on n'a pas reussit a faire
+    #les tas de Fibonacci car python ne permet pas de faire des pointeurs
+    #facilement -> Implémentation très difficile (le code qui a été testé pour
+    #les implémenté est dans le dossier github)
     P = [None for i in range(len(G))]
     d = [np.infty for i in range(len(G))]
     d[s] = 0
     F = File()
-    F.initialize(len(G)) #Initialisation (voir le fichier contenant les tas de fibonacci)
+    l = [(np.infty,i) for i in range(len(G))]
+    l[s]= (s,0)
+    for i in l:
+        F.add(i) #Initialisation (voir le fichier contenant les tas de fibonacci)
     while not F.empty():
+        #print(F.pop())
         u = F.pop()
         #Parcours des successeurs
         for (v,w) in G[u]:
@@ -17,3 +27,16 @@ def Dijkstra(G,s):
                 P[v] = u
                 F.modify(d[v],v)
     return d,P
+
+"""from importgraph import import_graph
+g, index_to_vertex_name = import_graph("graphs/CachanGraphe7.txt", True)
+#g = [[(1,0.2),(2,0.7)],[(2,0.4)],[(4,0.2),(5,0.8)],[],[],[]]
+(d,P) = Dijkstra(g,0)
+for t in range(100):
+    (dstar,Pstar) = Astar(g,0,t)
+    print(dstar[t],d[t])
+    if dstar[t] == d[t]:
+        print("pass",t)
+    else:
+        print("Error : ",t,dstar[t],d[t])
+"""
