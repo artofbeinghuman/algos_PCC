33c30474bc66d73dfe83b0de703a55be3f40a7a0
Lludion <45879010+Lludion@users.noreply.github.com>
2019-02-01 00:00:53 +0100

    Fibonacci Heaps

--- fibonacci -30_01.py
+++ fibonacci -30_01.py
@@ -23,12 +23,12 @@ A graph is an adjacency list, with |S| lists of couples (destination,weight).
 
 Operations on Fibonacci Heaps :
 
-insert			DONE
-accessmin		DONE
-deletemin		DONE
-decreasekey		DONE
-delete			DONE
-merge			DONE
+insert		DONE
+accessmin	DONE
+deletemin
+decreasekey	DONE
+delete		
+merge		DONE
 
 Copyright (c) 2018, Ulysse REMOND
 """
@@ -85,7 +85,7 @@ class Tree:
 	def __repr__(self):
 		"""0 means unmarked. The list of the subtrees is between brackets"""
 		return ("\n"+str(int(self.m))+"Tree:"+str(self.r.obj)+"("+str(self.v)+")")
-
+		
 class ref:
 	""" Simulation de pointeurs 
 	ref(obj) crée un pointeur vers obj
@@ -99,13 +99,6 @@ class ref:
 	def __repr__(self):
 		return str(self.obj)
 
-def removeobj(li, val):
-	"""removes the element of the list which points to val O(len(li))"""
-	for j in range(len(li)):
-		if li[j].obj == val:
-			del li[j]
-			return None
-	
 class Fibo:
 	"""
 	t: Dictionnaire d'arbres tournois, aux noeuds colorés dynamiquement
@@ -113,12 +106,10 @@ class Fibo:
 	"""
 	def __init__(self,value,maketree=True):
 		"""O(1) initializes the heap"""
-		self.d = dict()
 		if maketree:
 			self.t = [Tree(value)]
 		else:#it is already a tree
 			self.t = [value]
-			self.d[value] = self.t[0]
 		self.p = 0
 	def empty(self):
 		return self.t == []
@@ -140,22 +131,21 @@ class Fibo:
 	def insert(self,value):
 		"""O(1) inserts an Entry "ent" into the heap  """
 		if self.empty():#empty heap
-			self.__init__(value)
+			self.__init__()
 		else:
 			mini = self.minimalkey() #this is the min of the heap
 		
 			if mini > value.obj.key :
 				self.p = len(self.t) #the new element is the smallest
 			self.t.append(Tree(value))# adding a list of one tree with only one node
-			self.d[value] = self.t[-1]
+	
 	def merge(self,feap):
 		"""O(1) merges the heap with another Fibonacci heap named feap"""
 		mini = self.minimalkey()#this is the min of the heap
 		minf = feap.minimalkey()#This is the min of feap
 		if mini > minf :
 			self.p = len(self.t) + feap.p#changing minimum
-		self.t += feap.t             #merging feap
-		self.d = {**self.d, ** feap.d}
+		self.t += feap.t             #merging feap 
 
 	def decreasekey(self,ref_ent,new_key):
 		"""O(1) decreases the key of the root of the tree ref_ent """
@@ -168,7 +158,7 @@ class Fibo:
 		if ref_ent.f != None:#it is not the root
 			father = ref_ent.f# TODO see the section : "Remarques importantes"
 			if father.r.obj.key > new_key:#father has bigger key
-				removeobj(father.v,ref_ent)#severing the child tree
+				father.v.remove(ref_ent)#severing the child tree
 				self.merge(Fibo(ref_ent))#the new p is computed here
 				self.t[len(self.t)-1].m = False#ref_ent becomes an unmarked root
 				
@@ -191,7 +181,7 @@ class Fibo:
 		
 		ajout_fils = 0
 		for tree_pointer in minimal_tree.v:
-			#print("Fusion de " + str(tree_pointer.obj.r) + " et de ses fils")
+			print("Fusion de " + str(tree_pointer.obj.r) + " et de ses fils")
 			#ajout des fils directement dans la liste
 			# tree_pointer est un pointeur vers un arbre -> d'où le False
 			self.merge(Fibo(tree_pointer.obj,False))
@@ -200,17 +190,17 @@ class Fibo:
 			if tree_pointer.obj.r.obj.key < self.minimalkey() :
 				self.p = ajout_fils
 			ajout_fils += 1
-		print("Suppressing the entry:"+str(self.t[self.p].r.obj))
-		del self.t[ancien_p]#suppression du minimum -> O(n) pour l'instant
+		print("Suppressing the value "+str(self.t[self.p].r.obj.val))
+		del self.t[ancien_p]#suppression du minimum
 		
 		#compactage de la liste
 		pointers = dict()#tableau des (pointeurs,indice) , indicé par degré
 		
 		arbre_supp = 0
 		for tree_indice in range(len(self.t)):
-			#print(tree_indice,arbre_supp,tree_indice-arbre_supp,self.p)
+			print(tree_indice,arbre_supp,tree_indice-arbre_supp,self.p)
 			tree_indice -= arbre_supp
-			#print("Testing the value " + str(self.t[tree_indice].r) + "<? "+ str(self.minimalkey()))
+			print("Testing the value " + str(self.t[tree_indice].r) + "<? "+ str(self.minimalkey()))
 			#invariant : on ne peut supprimer que des arbres déja ajoutés
 			ok = False
 			while not ok:
@@ -221,14 +211,13 @@ class Fibo:
 					#il n'existe pas de degré identique
 					pointers[deg] = (ref(tree),tree_indice)
 					if tree.r.obj.key < self.minimalkey() :
-						#print("!!!!!!!!!!!!!"+str(self.t[tree_indice].r))
+						print("!!!!!!!!!!!!!"+str(self.t[tree_indice].r))
 						self.p = tree_indice
 				else:
 					self.t[tree_indice] = tree.fusionArbres(pointers[deg][0].obj)
 					
 					#supprime dans self l'arbre d'indice pointers[deg][1]
 					del self.t[pointers[deg][1]]
-					# TODO liste chainées pour permettre cette op. en O(1)
 					if pointers[deg][1] < self.p:
 						self.p -=1 
 					arbre_supp += 1
@@ -238,9 +227,7 @@ class Fibo:
 					
 					#pointers[deg] a disparu de Fibo
 					del pointers[deg]
-					ok = False#on refait un tour
-		#return pointers  #on peut admirer ainsi les différents arbres:
-		# pointers[i] = l'arbre de degré i
+					ok = False
 			
 	def delete(self,ref_ent):
 		"""deletes the values which is pointed by ref_ent, in O(log(n))"""
@@ -262,7 +249,7 @@ r2 = ref(s2)
 blop = Fibo(r1)
 s3 = Entry(60,3)
 r3 = ref(s3)
-s6 = Entry(600,6)#on insere des sommets
+s6 = Entry(600,6)
 r6 = ref(s6)
 blop.insert(r6)
 s7 = Entry(700,7)
@@ -294,15 +281,16 @@ if hard:
 	T = [-1 for _ in range(1001)]
 	S = [-1 for _ in range(1001)]
 	for t in range(9,1001):
-		S[t] = Entry(44449*44417*t % 1087,t)
+		S[t] = Entry(44449*44417*t % 1100,t)
 		T[t] = ref(S[t])
 		blop.insert(T[t])
-blop.deletemin()
 
-"""tests de deletemin
+"""tests de deletemin(
 blop.deletemin()
 blop.t[0].v[1].v[0].f.f
+"""
 
+"""
 Remarques importantes:
 
 # ligne 115:
@@ -314,4 +302,4 @@ ref_ent.r.obj.key est un entier (clef)
 ref_ent.f est un arbre, ou None
 ref_ent.f.r.obj.key est un entier (clef)
 self.t[self.p].r.obj.key est un entier
-self.t[self.p].v[1] est un pointeur vers un arbre"""
+"""
